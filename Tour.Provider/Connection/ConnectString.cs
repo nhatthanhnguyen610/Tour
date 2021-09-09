ae110dc1700edd69eb02b6dac4a173f37dd28f76
DuyTruong159 <1851050166truong@ou.edu.vn>
2021-09-09 21:49:09 +0700

    Doi Ip

--- Tour.Provider/Connection/ConnectString.cs
+++ Tour.Provider/Connection/ConnectString.cs
@@ -32,7 +32,7 @@ namespace Tour.Provider.Connection
                     {
                         Key ="DBConnection",
 
-                        Value ="Data Source=171.232.87.132,1433;Initial Catalog=Tour;User ID=sa;Password=123456;Connection Timeout=3000;"
+                        Value ="Data Source=115.73.235.205,1433;Initial Catalog=Tour;User ID=sa;Password=123456;Connection Timeout=3000;"
                     }
                 },
             };
