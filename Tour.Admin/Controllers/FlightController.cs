bd80c0b61a4484e8a8356b02620ec579f822208b
DuyTruong159 <1851050166truong@ou.edu.vn>
2021-09-26 09:38:32 +0700

    dang lam typeticket voi ticket

---
+++ Tour.Admin/Controllers/FlightController.cs
@@ -0,0 +1,240 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+using Tour.Admin.Models;
+using Tour.Base;
+using Tour.Common.Constants;
+using Tour.Service;
+
+namespace Tour.Admin.Controllers
+{
+    public class FlightController : BaseController
+    {
+        IFlightService _FlightService;
+        public FlightController()
+        {
+            _FlightService = new FlightService(string.Empty);
+        }
+        public IActionResult Index()
+        {
+            var vm = new FlightVM();
+            try
+            {
+                var model = new SysUsrUserFilterModel();
+                vm.p = vm.p == 0 ? 1 : vm.p;
+                model.pageIndex = vm.p;
+                model.pageSize = DefinedConstants.RowPerPage;
+                var _listFlight = _FlightService.GetList(model);
+                vm.ListFlight = _listFlight;
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return View(vm);
+        }
+
+        /// <summary>
+        /// _Index
+        /// </summary>
+        /// <param name="vm"></param>
+        /// <returns></returns>
+        public PartialViewResult _Index(FlightVM vm)
+        {
+            try
+            {
+                ViewBag.Paging = vm.p;
+                var viewModel = vm.ConvertObject<FlightVM, SysUsrUserFilterModel>();
+                viewModel.pageIndex = vm.p == 0 ? 1 : vm.p;
+                viewModel.pageSize = DefinedConstants.RowPerPage;
+                vm.ListFlight = _FlightService.GetList(viewModel);
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return PartialView(vm);
+        }
+
+        /// <summary>
+        ///  Author: dtr
+        ///  Description: Tạo chuyến bay mới
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public IActionResult _Create()
+        {
+            return View(new FlightViewModel());
+        }
+
+        /// <summary>
+        ///  Author: dtr
+        ///  Description:  Tạo chuyến bay mới
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> _Create(FlightViewModel vm)
+        {
+            var model = vm.ConvertObject<FlightViewModel, FlightModel>();
+            try
+            {
+                string uploads = string.Format("{0}{1}", PathServer.pathDir, PathServer.pathProduct);
+                foreach (var formFile in Request.Form.Files)
+                {
+                    if (formFile.Length > 0)
+                    {
+                        var fileName = DateTime.Now.ToString(PathServer.yyyyMMddhhmmssfff);
+                        var fileExtension = Path.GetExtension(formFile.FileName);
+                        string fileAvatar = fileName + fileExtension;
+                        string filePath = Path.Combine(uploads, fileAvatar);
+                        if (!Directory.Exists(Path.GetDirectoryName(uploads)))
+                        {
+                            Directory.CreateDirectory(Path.GetDirectoryName(uploads));
+                        }
+                        using (Stream fileStream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await formFile.CopyToAsync(fileStream);
+                        }
+                        model.image = string.Format("{0}{1}{2}", PathServer.pathProduct, fileAvatar);
+                    }
+                }
+                model.createdBy = Constants.UserCde;
+                if (!string.IsNullOrWhiteSpace(model.name))
+                {
+                    var submitResult = _FlightService.InsertFlight(model);
+                    return Json(new
+                    {
+                        IsSuccess = submitResult,
+                        Message = submitResult ? ResultStatus.SUCCESS : ResultStatus.FAIL
+                    });
+                }
+                else
+                {
+                    return Json(new
+                    {
+                        IsSuccess = false,
+                        Message = ResultStatus.FAIL
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    IsSuccess = false,
+                    Message = ResultStatus.ERROR
+                });
+            }
+        }
+
+        /// <summary>
+        ///  Author: dtr
+        ///  Description: cập nhật chuyến bay
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        public IActionResult _Update(decimal flightID)
+        {
+            var vm = new FlightViewModel();
+            try
+            {
+                var _lstFlight = _FlightService.GetInfo(flightID);
+                vm = _lstFlight.ConvertObject<FlightModel, FlightViewModel>();
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+            return View(vm);
+        }
+        /// <summary>
+        ///  Author: dtr
+        ///  Description: cập nhật chuyến bay
+        /// </summary>
+        /// <param name=""></param>
+        /// <returns></returns>
+        [AllowAnonymous]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> _Update(FlightViewModel vm)
+        {
+            var model = vm.ConvertObject<FlightViewModel, FlightModel>();
+            try
+            {
+                string uploads = string.Format("{0}{1}", PathServer.pathDir, PathServer.pathProduct);
+                foreach (var formFile in Request.Form.Files)
+                {
+                    if (formFile.Length > 0)
+                    {
+                        var fileName = DateTime.Now.ToString(PathServer.yyyyMMddhhmmssfff);
+                        var fileExtension = Path.GetExtension(formFile.FileName);
+                        string fileAvatar = fileName + fileExtension;
+                        string filePath = Path.Combine(uploads, fileAvatar);
+                        if (!Directory.Exists(Path.GetDirectoryName(uploads)))
+                        {
+                            Directory.CreateDirectory(Path.GetDirectoryName(uploads));
+                        }
+                        using (Stream fileStream = new FileStream(filePath, FileMode.Create))
+                        {
+                            await formFile.CopyToAsync(fileStream);
+                        }
+                        model.image = string.Format("{0}{1}{2}", PathServer.pathProduct, fileAvatar);
+                    }
+                }
+                model.createdBy = Constants.UserCde;
+                if (!string.IsNullOrWhiteSpace(model.name))
+                {
+                    var submitResult = _FlightService.UpdateFlight(model);
+                    return Json(new
+                    {
+                        IsSuccess = submitResult,
+                        Message = submitResult ? ResultStatus.SUCCESS : ResultStatus.FAIL
+                    });
+                }
+                else
+                {
+                    return Json(new
+                    {
+                        IsSuccess = false,
+                        Message = ResultStatus.FAIL
+                    });
+                }
+            }
+            catch (Exception ex)
+            {
+                return Json(new
+                {
+                    IsSuccess = false,
+                    Message = ResultStatus.ERROR
+                });
+            }
+        }
+        /// <summary>
+        /// DeleteSysNews
+        /// </summary>
+        /// <param name="pFlightID"></param>
+        /// <param name="pRowVersion"></param>
+        /// <returns></returns>
+        [HttpPost]
+        public JsonResult Delete(decimal pFlightID)
+        {
+            FlightModel model = new FlightModel()
+            {
+                Id = pFlightID,
+            };
+            var submitResult = _FlightService.DeleteFlight(model);
+            return Json(new
+            {
+                IsSuccess = submitResult,
+                Message = submitResult ? ResultStatus.SUCCESS : ResultStatus.FAIL
+            });
+        }
+    }
+}
---
+++ Tour.Provider/Implementation/FlightProvider.cs
@@ -0,0 +1,130 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tour.Base;
+using Tour.Common.Constants;
+using Tour.Provider.Connection;
+using static Tour.Provider.Connection.ConnectString;
+
+namespace Tour.Provider
+{
+    public class FlightProvider : ConnectSqlExecute, IFlightProvider
+    {
+        public FlightProvider(string appId, string userId)
+            : base(ConnectCode.DBConnection, appId, userId)
+        {
+
+        }
+        /// <summary>
+        /// CreateBy: dtr
+        /// Description: Lấy danh sách chuyến bay
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public List<FlightModel> GetList(SysUsrUserFilterModel model)
+        {
+            var paramObj = new object[]
+            {
+                model.keyWord,
+                model.pageIndex,
+                model.pageSize
+            };
+            var resultComment = base.ExecProcedure<FlightModel>("sp_Flight_GetList_V01", paramObj);
+            if (resultComment.Any())
+            {
+                return resultComment.ToList();
+            }
+            return null;
+
+        }
+        /// <summary>
+        /// CreateBy: dtr
+        /// Description: Thêm chuyến bay
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public bool InsertFlight(FlightModel model)
+        {
+            var paramObj = new object[]
+            {
+                model.name,
+                model.image,
+                model.description,
+                model.date,
+                model.duaration,
+                model.createdBy
+            };
+            var result = base.ExeScalar("sp_Flight_Insert_V01", paramObj);
+            if (result != null && result.ToString().Equals("1"))
+            {
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// CreateBy: dtr
+        /// Description: Lấy thông tin chuyến bay
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public FlightModel GetInfo(decimal Id)
+        {
+            var paramObj = new object[]
+            {
+               Id
+            };
+            var result = base.ExecProcedure<FlightModel>("sp_Flight_GetInfo_V01", paramObj);
+            if (result.Any())
+            {
+                return result.FirstOrDefault();
+            }
+            return new FlightModel();
+        }
+        /// <summary>
+        /// CreateBy: dtr
+        /// Description: Cập nhật chuyến bay
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public bool UpdateFlight(FlightModel model)
+        {
+            var paramObj = new object[]
+            {
+                model.Id,
+                model.name,
+                model.image,
+                model.description,
+                model.date,
+                model.duaration,
+                model.createdBy
+            };
+            var result = base.ExeScalar("sp_Flight_Update_V01", paramObj);
+            if (result != null && result.ToString().Equals(ResultCode.Success))
+            {
+                return true;
+            }
+            return false;
+        }
+        /// <summary>
+        /// CreateBy: dtr
+        /// Description: Xóa chuyến bay
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        public bool DeleteFlight(FlightModel model)
+        {
+            var paramObj = new object[]
+            {
+                model.Id
+            };
+            var result = base.ExeScalar("sp_Flight_Delete_V01", paramObj);
+            if (result != null)
+            {
+                return true;
+            }
+            return false;
+        }
+    }
+}
