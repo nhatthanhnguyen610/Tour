70da30ccad27d69ddadbdb5493be7c45a0880e0c
Nguyễn Nhật Thanh <52287639+nhatthanhnguyen610@users.noreply.github.com>
2021-09-28 16:57:35 +0700

    jj
    ygy

---
+++ Tour.Service/Interface/ITypeTicketService.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Tour.Base;
+
+namespace Tour.Service
+{
+    public interface ITypeTicketService
+    {
+        /// <summary>
+        /// CreateBy: dtr
+        /// Description: Lấy danh sách loại vé
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        List<TypeTicketModel> GetList(SysUsrUserFilterModel model);
+        /// <summary>
+        /// CreateBy: dtr
+        /// Description: Thêm loại vé
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        bool InsertTypeTicket(TypeTicketModel model);
+        /// <summary>
+        /// CreateBy: dtr
+        /// Description: Lấy thông tin loại vé
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        TypeTicketModel GetInfo(decimal Id);
+        /// <summary>
+        /// CreateBy: dtr
+        /// Description: Cập nhật loại vé
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        bool UpdateTypeTicket(TypeTicketModel model);
+        /// <summary>
+        /// CreateBy: dtr
+        /// Description: Xóa loại vé
+        /// </summary>
+        /// <param name="model"></param>
+        /// <returns></returns>
+        bool DeleteTypeTicket(TypeTicketModel model);
+    }
+}
